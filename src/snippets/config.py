4fd4c049f4a58c352e8ac08c52f01f27b8c11cbb
kihiyuki <39235239+kihiyuki@users.noreply.github.com>
2022-11-23 16:08:05 +0900

    initial

---
+++ src/snippets/config.py
@@ -0,0 +1,194 @@
+# https://github.com/kihiyuki/python-snippets
+# MIT License
+import shutil
+from configparser import ConfigParser, DEFAULTSECT
+from pathlib import Path
+from typing import Optional, Union
+
+
+__version__ = "1.0.0"
+
+
+class ConfigData(dict):
+    def __init__(self, data: dict, section: Optional[str] = None):
+        if self._have_section(data):
+            if section is None:
+                pass
+            elif section not in data:
+                data[section] = dict()
+        else:
+            if section is None:
+                raise ValueError("ConfigData must have section")
+            else:
+                data = {section: data}
+        return super().__init__(data)
+
+    def to_dict(self) -> dict:
+        return dict(self)
+
+    @staticmethod
+    def _have_section(data: dict) -> bool:
+        have = True
+        for v in data.values():
+            if not isinstance(v, dict):
+                have = False
+                break
+        return have
+
+
+class config(object):
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def load(
+        file: Union[str, Path] = "./config.ini",
+        section: Optional[str] = None,
+        encoding: Optional[str] = None,
+        notfound_ok: bool = False,
+        default: Optional[dict] = None,
+        cast: bool = False,
+        strict_cast: bool = False,
+        strict_key: bool = False,
+    ) -> dict:
+        """Read configuration file and convert its section to dict.
+
+        Args:
+            file (str or Path, optional): Configuration file path
+            section (str, optional): Section (If None, load all sections)
+            encoding (str, optional): File encoding
+            notfound_ok (bool, optional): If True, return empty dict.
+            default (dict, optional): Default values of config
+            cast (bool, optional): If True, cast to type of default value.
+            strict_cast (bool, optional): If False, cast as much as possible.
+            strict_key (bool, optional): If False, keys can be added.
+
+        Returns:
+            dict
+
+        Raises:
+            FileNotFoundError: If `notfound_ok` is False and `file` not found.
+            ValueError: If `strict_cast` is True and failed to cast.
+            KeyError: If `strict_key` is True and some keys of configfile is not in default.
+        """
+        filepath = Path(file)
+        config_ = ConfigParser()
+        if filepath.is_file():
+            with filepath.open(mode="r", encoding=encoding) as f:
+                config_.read_file(f)
+        elif not notfound_ok:
+            raise FileNotFoundError(file)
+
+        allsection = section is None
+        if allsection:
+            sections_file = config_.sections()
+            if len(dict(config_[DEFAULTSECT])) > 0:
+                sections_file = [DEFAULTSECT] + sections_file
+        else:
+            sections_file = [section]
+        sections = sections_file.copy()
+
+        if default is None:
+            default = dict()
+        else:
+            default = ConfigData(default, section=section).to_dict()
+            for k in default.keys():
+                if k not in sections:
+                    sections.append(k)
+
+        data = dict()
+        for s in sections:
+            if s in default:
+                data[s] = default[s].copy()
+            else:
+                data[s] = dict()
+            if s not in sections_file:
+                continue
+            for k, v in dict(config_[s]).items():
+                if k in data[s]:
+                    if cast:
+                        try:
+                            # cast to type(default[k])
+                            v = type(data[s][k])(v)
+                        except ValueError as e:
+                            if strict_cast:
+                                raise ValueError(e)
+                elif strict_key:
+                    raise KeyError(k)
+                data[s][k] = v
+
+        if not allsection:
+            data = data[section]
+
+        return data
+
+    @staticmethod
+    def save(
+        data: dict,
+        file: str = "./config.ini",
+        section: str = None,
+        encoding: Optional[str] = None,
+        mode: str = "interactive",
+        backup: bool = True,
+        backup_suffix: str = "~",
+    ) -> None:
+        """Save configuration dict to file.
+
+        Args:
+            data (dict): Configuration data (supports single/multiple sections)
+            file (str or Path, optional): Configuration file path
+            section (str, optional): Section (if single-section data)
+            encoding (str, optional): File encoding
+            mode (str, optional): 'interactive', 'overwrite', 'add', 'leave'
+            exist_ok (bool, optional): If False and file exists, raise an error.
+            overwrite (bool, optional): If True and file exists, overwrite.
+            backup (bool, optional): If True and file exeists, create backup file.
+            backup_suffix (str, optional): Suffix of backup file name
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: If `mode` is unknown
+        """
+        filepath = Path(file)
+        config_ = ConfigParser()
+
+        data = ConfigData(data, section=section).to_dict()
+        if section is not None:
+            # use only specified section
+            data = {section: data[section]}
+
+        write = True
+        if filepath.is_file():
+            mode = mode.lower()
+            if mode in ["i", "interactive"]:
+                mode = input(f"'{filepath.name}' already exists --> ([o]verwrite/[a]dd/[l]eave/[c]ancel)?: ").lower()
+            if mode in ["o", "overwrite"]:
+                pass
+            elif mode in ["a", "add"]:
+                # always load all sections
+                data = config.load(
+                    file = file,
+                    section = None, 
+                    encoding = encoding,
+                    default = data,
+                    cast = False,
+                    strict_key = False)
+            elif mode in ["l", "leave", "c", "cancel", "n", "no"]:
+                write = False
+                backup = False
+            else:
+                raise ValueError(f"Unknown mode '{mode}'")
+            if backup:
+                config._backup(file=file, suffix=backup_suffix)
+        if write:
+            config_.read_dict(data)
+            with filepath.open(mode="w", encoding=encoding) as f:
+                config_.write(f)
+        return None
+
+    @staticmethod
+    def _backup(file: str, suffix: str = "~"):
+        file_back = str(file) + suffix
+        shutil.copyfile(file, file_back)
---
+++ tests/test_config.py
@@ -0,0 +1,143 @@
+from random import sample
+import shutil
+from pathlib import Path
+
+import pytest
+
+from src.snippets import config as configlib
+
+
+VERBOSE = True
+TEMPDIR = "./testtemp"
+CONFIGFILE = TEMPDIR + "/config.ini"
+
+
+@pytest.fixture(scope="class", autouse=True)
+def tempdir():
+    tempdirpath = Path(TEMPDIR).resolve()
+    if tempdirpath.is_dir():
+        shutil.rmtree(str(tempdirpath))
+    tempdirpath.mkdir(exist_ok=False)
+
+    yield None
+
+    shutil.rmtree(str(tempdirpath))
+
+
+@pytest.fixture(scope="function", autouse=False)
+def sampleconfig():
+    config =  dict(
+        DEFAULT = dict(
+            x = "dx",
+        ),
+        a = dict(
+            x = "ax",
+            y = "ay",
+            n = "1",
+        ),
+        b = dict(
+            x = "bx",
+            y = "by",
+            n = "2",
+        ),
+    )
+    configlib.config.save(config, file=CONFIGFILE, mode="overwrite")
+
+    yield config
+
+    Path(CONFIGFILE).unlink()
+
+
+class TestConfig(object):
+    def test_load(self, sampleconfig):
+        config_load = configlib.config.load(file=CONFIGFILE)
+        assert config_load == sampleconfig
+
+        with pytest.raises(FileNotFoundError):
+            # notfound_ok = False
+            _ = configlib.config.load(file=".INVALID")
+        config_load = configlib.config.load(file=".INVALID", notfound_ok=True)
+        assert config_load == dict()
+
+    def test_load_section(self, sampleconfig):
+        config_load = configlib.config.load(file=CONFIGFILE, section="a")
+        assert config_load == sampleconfig["a"]
+
+        with pytest.raises(KeyError):
+            _ = configlib.config.load(file=CONFIGFILE, section="c")
+
+    def test_load_default(self, sampleconfig):
+        default = dict(n=11, m=12)
+        config_load = configlib.config.load(file=CONFIGFILE, section="a", default=default)
+        config_load2 = configlib.config.load(file=CONFIGFILE, default={"a": default})
+        c = sampleconfig.copy()
+        c["a"]["m"] = 12
+        assert config_load == c["a"]
+        assert config_load2 == c
+
+        # strict_key
+        with pytest.raises(KeyError):
+            _ = configlib.config.load(file=CONFIGFILE, default={"a": default}, strict_key=True)
+
+        # strict_cast
+        with pytest.raises(ValueError):
+            _ = configlib.config.load(file=CONFIGFILE, default={"a": dict(x=0)}, cast=True, strict_cast=True)
+
+        # cast
+        config_load = configlib.config.load(file=CONFIGFILE, default={"a": default}, cast=True)
+        c = sampleconfig.copy()
+        c["a"]["n"] = int(c["a"]["n"])
+        c["a"]["m"] = 12
+        assert config_load == c
+
+    def test_save(self, sampleconfig):
+        # invalid mode
+        config = dict(hoge=dict(fuga=5))
+        with pytest.raises(ValueError):
+            configlib.config.save(config, file=CONFIGFILE, mode="x")
+
+    @pytest.mark.parametrize("mode", ["o", "OVERWRITE"])
+    def test_save_overwrite(self, sampleconfig, mode):
+        config = dict(hoge=dict(fuga=5))
+        config_str = dict(hoge=dict(fuga="5"))
+        configlib.config.save(config, file=CONFIGFILE, mode=mode)
+        config_load = configlib.config.load(file=CONFIGFILE)
+        assert config_load == config_str
+
+    @pytest.mark.parametrize("mode", ["a", "add"])
+    def test_save_add(self, sampleconfig, mode):
+        config = dict(a=dict(x="addx", z="addz"), b=dict(y="addy"))
+        configlib.config.save(config, file=CONFIGFILE, mode=mode)
+        config_load = configlib.config.load(file=CONFIGFILE)
+        sampleconfig["a"]["z"] = "addz"
+        assert config_load == sampleconfig
+
+    @pytest.mark.parametrize("section", [None, "a", "c"])
+    @pytest.mark.parametrize("has_section", [False, True])
+    def test_save_add_param(self, sampleconfig, section, has_section):
+        config = dict(x="addx", z="addz")
+        if section is None:
+            with pytest.raises(ValueError):
+                configlib.config.save(config, file=CONFIGFILE, mode="a", section=section)
+        else:
+            configlib.config.save(
+                {section: config} if has_section else config,
+                file=CONFIGFILE,
+                mode="a",
+                section=None if has_section else section,
+            )
+            config_load = configlib.config.load(file=CONFIGFILE)
+            c = sampleconfig.copy()
+            if section == "c":
+                c[section] = dict()
+                c[section]["x"] = "addx"
+            c[section]["z"] = "addz"
+            assert c == config_load
+
+    @pytest.mark.parametrize("mode", ["l", "leave", "c", "cancel", "n", "no"])
+    def test_save_leave(self, sampleconfig, mode):
+        config = dict(hoge=dict(fuga="5"))
+        configlib.config.save(config, file=CONFIGFILE, mode=mode)
+        config_load = configlib.config.load(file=CONFIGFILE)
+        assert config_load != config
+        assert config_load == sampleconfig
